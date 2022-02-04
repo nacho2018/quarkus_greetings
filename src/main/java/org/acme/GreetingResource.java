68f3014b9c4c758b4dfffa43e30fd4a295ad9ac9
nacho2018 <joseigvillanueva@hotmail.com>
2022-02-04 15:07:29 +0100

    update file

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -59,9 +59,8 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     @Path("/greetings/list")
     public List<String> list(){
-        System.out.println("changes from local");
-        System.out.println("changes from remote");
-        System.out.println("changes from remote 2");
-        return listOfGreetings;
+       System.out.println("changes from local");
+       System.out.println("changes from local 2");
+    return listOfGreetings;
     }
 }
